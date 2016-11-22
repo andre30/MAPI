e2a3f0d016d89593f15f055ad4f3a2120be2aadb
andre30 <andreapiccolo95@gmail.com>
2016-11-22 16:21:34 +0100

    timer con uif

--- main.c
+++ main.c
@@ -1,41 +1,52 @@
 #include <stm32f30x.h>
-#define TIMINT 1
-#define FCK  (uint32_t)72000000
-#define TIM2EN (uint32_t)0x00000001   
-#define CEN (uint32_t)0x0001
-#define UIF (uint32_t)0x00000001
-#define PE815OUT (uint32_t)0x55550000
-#define FCKS 72000000
+#define TIMINT (uint32_t)1
+#define TIM2EN (uint32_t)0x1
+#define FCK 72000000
 #define FCKMIL 72000
 #define FCKMIC 72
-   int count;
-  int secondi=0;
-  int milli=0;
-  int micro=0;
-void main(void)
-{
-  RCC->AHBENR |= 1<<21; //attivo porta E
+#define UIF (uint32_t)0x1
+int secondi=0;
+int milli=0;
+int micro;
+unsigned int count=0;
+double countmil=0;
+double countmic=0;
+
+
+void main(void){
+  
+  RCC->AHBENR |= 1<<21;
   RCC->AHBENR |=1<<17; //abilito porta A
-  RCC->APB1ENR |= TIM2EN;//ABILITO TIMER 2 HA COME MAX FREQ DI FUNZIONAMENTO META' DI QUELLA DEL CLOCK
-  GPIOE ->MODER |= PE815OUT; 
-   GPIOA->MODER &= ~ ((unsigned int)3);//abilito pin da 8 a 15
-  //configurazione timer
-  TIM2->PSC=0;
-  TIM2->CNT =0; //TIMINT è il numero di secondi che voglio attivare, scrivo 1 secondo nell' arr
-  TIM2->CR1=0;
-  //attendo che un secondo sia trascorso
-  while(1)
-  {
+  GPIOE->MODER |= 0x55550000;
+  GPIOA->MODER &= ~ ((unsigned int)3); //00 nei due bit meno significativi
+  RCC->APB1ENR |= 0x1; //abilito TIM2
+  TIM2->CR1 = 0; //Registro di controllo in cui inizialmente disabilito tutte le opzioni su TIM6
+  TIM2->CR2 =0;
+  TIM2->PSC = 0;
+  TIM2->CNT=0; //imposto conteggio a 0
+  TIM2->ARR = (uint32_t) TIMINT*FCKMIC-1; //fisso il registro di arr al massimo
+  
+  while(1){
+    //questo if serve a capire se il tasto è stato premuto. Nel registro IDR il bit 0 è collegato al pin PA0
     while(GPIOA->IDR & 0x1 == 0x1){ 
       if((TIM2->CR1 &0x1) != 0x1){
         TIM2->CR1 |= (uint32_t)0x1; //CEN abilito conteggio
         GPIOE->ODR |= (uint32_t)0x0000FF00;
       }
-      count=TIM2->CNT;
-      secondi=count/FCKS;
-      micro=count/FCKMIC;
-      milli=count/FCKMIL;
-      TIM2->CR1 &=0x0;
+      if((TIM2->SR & UIF)==UIF){ //ogni secondo//microsecondo scatta l' interrupt e si alza il flag in uif => incremento il conteggio
+        TIM2->SR &= ~ UIF;
+        (count)++;
+        secondi=count/1000000;
+        milli=count/1000;
+        micro=count;
+        
+        
+      }
     }
-  }   
+    
+    TIM2->CR1 &=~((unsigned int)1); //disabilito conteggio
+    GPIOE->ODR &= 0x0;
+     
+      
+    }
 }
